05768cb72719e59259d12221c8947b4b92c27478
Sergey Smirnov <30383600+smirnov-sergey@users.noreply.github.com>
2020-10-01 22:42:35 +0300

    #B06L01 - lessons 1-6

---
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './AnswerItem.css';
+
+const AnswerItem = props => {
+
+    return (
+        <li className="AnswerItem">
+            {props.answer.text}
+        </li>
+    )
+}
+
+export default AnswerItem
